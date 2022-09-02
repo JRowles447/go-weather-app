85fcf2598de18613fcd9ec091175700e560a044b
Jason Rowley <jrowles441@gmail.com>
2022-09-01 18:24:17 -0700

    adding more zip validation tests

--- app/weather.go
+++ app/weather.go
@@ -4,11 +4,6 @@ import (
 	"regexp"
 )
 
-// InitializeApp initializes values for application to run.
-func InitializeApp() {
-
-}
-
 // RetrieveZip retrieves a zip from the user
 func RetrieveZip() {
 
@@ -21,7 +16,7 @@ func QueryWeather() {
 
 // ValidateZip ensures the zip code has a valid format
 func ValidateZip(zip string) bool {
-	regex := regexp.MustCompile(`\d{5}`)
+	regex := regexp.MustCompile(`^\d{5}$`)
 
 	return regex.Match([]byte(zip))
 }
--- app/weather_test.go
+++ app/weather_test.go
@@ -5,10 +5,7 @@ import (
 	"testing"
 )
 
-// const invalidZipsDigits := []string{"12", "557852", "0", "69621475"}
-// const invalidZipsChars := []string{"foo", "another", ""}
-
-func TestValidateZip(t *testing.T) {
+func TestValidateZip_Valid(t *testing.T) {
 	var tests = []struct {
 		zip  string
 		want bool
@@ -17,6 +14,56 @@ func TestValidateZip(t *testing.T) {
 		{"98109", true},
 		{"02210", true},
 		{"02144", true},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%s", tt.zip)
+
+		t.Run(testName, func(t *testing.T) {
+			res := ValidateZip(tt.zip)
+			if res != tt.want {
+				t.Errorf("got %t, want %t", res, tt.want)
+			}
+		})
+	}
+}
+
+// Contains mix of valid and invalid digit strings
+func TestValidateZip_InvalidDigits(t *testing.T) {
+	var tests = []struct {
+		zip  string
+		want bool
+	}{
+		{"12", false},
+		{"557852", false},
+		{"90000", true},
+		{"0", false},
+		{"69621475", false},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%s", tt.zip)
+
+		t.Run(testName, func(t *testing.T) {
+			res := ValidateZip(tt.zip)
+			if res != tt.want {
+				t.Errorf("got %t, want %t", res, tt.want)
+			}
+		})
+	}
+}
+
+// Contains a mix of valid zips and invalid character strings
+func TestValidateZip_InvalidCharacters(t *testing.T) {
+	var tests = []struct {
+		zip  string
+		want bool
+	}{
+		{"foo", false},
+		{"another", false},
+		{"", false},
+		{"bar", false},
+		{"12345", true},
 	}
 
 	for _, tt := range tests {
